6072bcfa44b551bf5d986546f9c61eaca271cd12
Shinelia <a.wech62@gmail.com>
2020-07-02 13:48:30 +0200

    mise à jour ville ok

--- src/Period.js
+++ src/Period.js
@@ -11,12 +11,12 @@ class Period extends Component {
         let period = this.props.period;
         console.log(period);
         let iconURL = `http://openweathermap.org/img/wn/${period.weather[0].icon}@2x.png`;        
-        
+
         return ( 
         <div className="period">
             <span className="period_date">{period.dt_txt}</span>
             <img className="period_image" src={iconURL} alt=""></img>
-            <span className="period_temp">{period.main.temp}</span>
+            <span className="period_temp">{Math.round(period.main.temp)}°C</span>
             <span className="period_description">{period.weather[0].description}</span>
             
         </div>
--- src/WeatherSystem.js
+++ src/WeatherSystem.js
@@ -7,7 +7,6 @@ class WeatherSystem extends Component {
     state = {
         periods: [],
         city: "Lens",
-        cityName : ""
 
      }
 
@@ -24,25 +23,24 @@ class WeatherSystem extends Component {
 
     changeCity = (e) =>{
         e.preventDefault();
-        this.setState({
-            city : this.state.cityName
+        let city = this.state.city;
+        axios.get('https://api.openweathermap.org/data/2.5/forecast?q='+city+'&lang=fr&units=metric&appid=8c3a54c385c9c9d874d88f2cd6b3dda8')
+        .then(res => {
+            this.setState({
+                periods: res.data.list,
+                city : this.state.city
+            })
         })
     }
     
     handleChange = (e) => {
         this.setState({
-            cityName : e.target.value
+            city : e.target.value
         })
     }
 
     render() { 
-        console.log(this.state.periods);
-        // periodsList = [...periodsList,this.state.periods]
-        //     .filter(period => period.dt_txt.substr(11) === "12:00:00")
-        //     .map(period => {
-        //         return <Period period={period} />
-        // })
-
+   
         let periodTemp = this.state.periods.filter(period => period.dt_txt.substr(11) === "12:00:00");
         let periodsList = [this.state.periods[0], ...periodTemp];
         let res =  periodsList.map(period => period != null ? <Period period={period}/> : null );
@@ -51,12 +49,12 @@ class WeatherSystem extends Component {
             <div className="weathers">
                 <div className = "choice">
                     <form onSubmit={this.changeCity} >
-                        <input type="text" id="city" placeholder="Choose a city" onChange={this.handleChange}></input>
+                        <input type="text" id="city" placeholder="Choose a city" name={this.state.city} onChange={this.handleChange}></input>
                         <button type="submit">Search</button>
                     </form>
                 </div>
                 <h1>{this.state.city}</h1>
-                {res}
+                <div className="resultat">{res}</div>
             </div>
          );
     }
